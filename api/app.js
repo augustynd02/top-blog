ca17cfc535cb488c13b2561e381856efeb0dbc24
yurishe0 <yurishe00@gmail.com>
2024-12-17 16:49:49 +0100

    feat: configure CORS for requests between client and server

--- api/app.js
+++ api/app.js
@@ -1,11 +1,14 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const path = require("node:path");
 
 const postsRouter = require('./routes/postsRouter');
 
 const app = express();
 
+// TODO: configure CORS for deployment
+app.use(cors());
 app.use(express.static(__dirname + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
