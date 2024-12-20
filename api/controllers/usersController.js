f8707308ab8cc9706f0fbe10007e76953fe8e40d
yurishe0 <yurishe00@gmail.com>
2024-12-20 16:18:30 +0100

    feat: implement user registration

--- api/controllers/usersController.js
+++ api/controllers/usersController.js
@@ -1,10 +1,26 @@
 const bcrypt = require('bcryptjs');
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
 
 const usersController = {
     createUser: async (req, res) => {
-        const data = req.body;
-        data.password = await bcrypt.hash(data.password, 10);
-        console.log(data);
+        try {
+            req.body.password = await bcrypt.hash(req.body.password, 10);
+            const user = await prisma.user.create({
+                data: {
+                    username: req.body.username,
+                    password: req.body.password,
+                    role_id: 1
+                }
+            })
+            res.status(201).json({ message: 'User created successfuly.' })
+        } catch(err) {
+            console.error(err);
+            if (err.code == 'P2002') {
+                return res.status(409).json({ message: 'This username is already taken.'});
+            }
+            res.status(500).json({ message: 'An error has occured while creating the user.'});
+        }
     }
 }
 
--- client/src/pages/RegisterPage/RegisterPage.jsx
+++ client/src/pages/RegisterPage/RegisterPage.jsx
@@ -3,22 +3,60 @@ import useDocumentTitle from '../../hooks/useDocumentTitle';
 import Header from '../../components/Header/Header';
 import Footer from '../../components/Footer/Footer';
 
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import styles from './registerpage.module.css';
+
 function RegisterPage() {
     useDocumentTitle('register');
+    const navigate = useNavigate();
+
+    const [formData, setFormData] = useState({ username: '', password: '' });
+    const [error, setError] = useState(null);
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData({ ...formData, [name]: value })
+    }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        try {
+            const response = await fetch('http://localhost:3000/api/users', {
+                method: 'POST',
+                headers: { 'Content-type': 'application/json' },
+                body: JSON.stringify(formData),
+            });
+
+            console.log(response);
+
+            if (response.ok) {
+                navigate('/login')
+            } else {
+                const data = await response.json();
+                setError(data.message || 'Unknown error.');
+            }
+        } catch(err) {
+            setError('Failed to connect to the server: ' + err);
+        }
+    }
     return (
         <>
             <Header />
             <main>
                 <h2>Register</h2>
-                <form action="http://localhost:3000/api/users" method="POST">
+                <form onSubmit={handleSubmit}>
                     <label htmlFor="username">Username</label>
-                    <input type="text" name="username" id="username" />
+                    <input type="text" name="username" id="username" onChange={handleChange} />
 
                     <label htmlFor="password">Password</label>
-                    <input type="text" name="password" id="password" />
+                    <input type="text" name="password" id="password" onChange={handleChange} />
 
                     <button type="submit">Submit</button>
                 </form>
+                {error && <p className={styles.error}>{error}</p>}
             </main>
             <Footer />
         </>
