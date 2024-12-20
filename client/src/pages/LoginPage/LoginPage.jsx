5fbd890285cce2918f1a9c83c208fd955384e688
yurishe0 <yurishe00@gmail.com>
2024-12-20 23:10:04 +0100

    feat: add login page

--- client/src/pages/LoginPage/LoginPage.jsx
+++ client/src/pages/LoginPage/LoginPage.jsx
@@ -1,6 +1,64 @@
+import useDocumentTitle from '../../hooks/useDocumentTitle';
+
+import Header from '../../components/Header/Header';
+import Footer from '../../components/Footer/Footer';
+
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import styles from './loginpage.module.css';
+
 function LoginPage() {
+    useDocumentTitle('login');
+    const navigate = useNavigate();
+
+    const [formData, setFormData] = useState({ username: '', password: '' });
+    const [error, setError] = useState(null);
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData({ ...formData, [name]: value })
+    }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        try {
+            const response = await fetch('http://localhost:3000/api/auth/login', {
+                method: 'POST',
+                headers: { 'Content-type': 'application/json' },
+                body: JSON.stringify(formData),
+            });
+
+            const data = await response.json();
+
+            if (response.ok) {
+                navigate('/')
+            } else {
+                setError(data.message || 'Unknown error.');
+            }
+        } catch(err) {
+            setError('Failed to connect to the server: ' + err);
+        }
+    }
     return (
-        <h1>login page</h1>
+        <>
+            <Header />
+            <main>
+                <h2>Login</h2>
+                <form onSubmit={handleSubmit}>
+                    <label htmlFor="username">Username</label>
+                    <input type="text" name="username" id="username" onChange={handleChange} />
+
+                    <label htmlFor="password">Password</label>
+                    <input type="text" name="password" id="password" onChange={handleChange} />
+
+                    <button type="submit">Submit</button>
+                </form>
+                {error && <p className={styles.error}>{error}</p>}
+            </main>
+            <Footer />
+        </>
     )
 }
 
