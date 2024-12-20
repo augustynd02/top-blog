394609c0b83b0e6a438601b270e78818757d8944
yurishe0 <yurishe00@gmail.com>
2024-12-20 16:46:43 +0100

    misc: store user's id in jwt for security

--- api/utils/generateAccessToken.js
+++ api/utils/generateAccessToken.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken');
 
-const generateAccessToken = (username) => {
-    return jwt.sign(username, process.env.TOKEN_SECRET, { expiresIn: 1000 * 60 * 60 * 24 * 7})
+const generateAccessToken = (id) => {
+    return jwt.sign(id, process.env.TOKEN_SECRET, { expiresIn: 1000 * 60 * 60 * 24 * 7})
 }
 
 module.exports = generateAccessToken;
