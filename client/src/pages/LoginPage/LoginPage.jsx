90a9aa39f70f026a58cffc84844cd9cef7976be1
yurishe0 <yurishe00@gmail.com>
2024-12-21 14:53:05 +0100

    fix: allow the browser to save the cookie from the server

--- client/src/pages/LoginPage/LoginPage.jsx
+++ client/src/pages/LoginPage/LoginPage.jsx
@@ -27,6 +27,7 @@ function LoginPage() {
             const response = await fetch('http://localhost:3000/api/auth/login', {
                 method: 'POST',
                 headers: { 'Content-type': 'application/json' },
+                credentials: 'include',
                 body: JSON.stringify(formData),
             });
 
