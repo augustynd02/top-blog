45e0bd1e3bbb03aa82f01956a9215fe0b5a18668
yurishe0 <yurishe00@gmail.com>
2024-12-21 16:59:11 +0100

    fix: rerun the effect after user changes

--- client/src/hooks/useAuth.jsx
+++ client/src/hooks/useAuth.jsx
@@ -23,9 +23,9 @@ function useAuth() {
             }
         }
         checkLoginStatus();
-    }, [])
+    }, [user])
 
-    return user;
+    return { user, setUser};
 }
 
 export default useAuth;
