f639e4f2100bdc4c6f956ccc461081df87e98482
yurishe0 <yurishe00@gmail.com>
2024-12-23 17:59:12 +0100

    feat: layout and style admin page

--- client/src/pages/AdminPage/AdminPage.jsx
+++ client/src/pages/AdminPage/AdminPage.jsx
@@ -1,6 +1,38 @@
+import useDocumentTitle from '../../hooks/useDocumentTitle';
+import useAuth from '../../hooks/useAuth';
+
+import Header from '../../components/Header/Header';
+import Footer from '../../components/Footer/Footer';
+
+import styles from './AdminPage.module.css';
+
+import addIcon from '../../assets/icons/add.svg';
+import analyticsIcon from '../../assets/icons/analytics.svg';
+import listIcon from '../../assets/icons/list.svg';
+
 function AdminPage() {
+    useDocumentTitle('admin')
+    const { user } = useAuth();
+    if (!user || user.role_id != 2) {
+        return <div>Not an admin</div>
+    }
     return (
-        <div>admin</div>
+        <>
+            <Header />
+            <div className={styles.adminWrapper}>
+                <div className={styles.actionsContainer}>
+                    <ul>
+                        <li><img src={addIcon} alt="Add icon" /></li>
+                        <li><img src={listIcon} alt="List icon" /></li>
+                        <li><img src={analyticsIcon} alt="Analytics icon" /></li>
+                    </ul>
+                </div>
+                <main>
+
+                </main>
+            </div>
+            <Footer />
+        </>
     )
 }
 
