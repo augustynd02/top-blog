0ebd53635aec2cac1ac0082673b52ca77757419f
yurishe0 <yurishe00@gmail.com>
2024-12-27 13:56:35 +0100

    feat: log incoming requests

--- api/app.js
+++ api/app.js
@@ -21,6 +21,10 @@ app.use(express.static(__dirname + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(authenticateToken)
+app.use((req, res, next) => {
+    console.log(req.method, req.url);
+    next()
+})
 
 app.use('/api/posts', postsRouter);
 app.use('/api/users', usersRouter);
