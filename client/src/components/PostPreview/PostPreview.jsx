de9be7a63df5c713fe044a29ec5bb42ed9d5c81f
yurishe0 <yurishe00@gmail.com>
2025-01-02 15:28:46 +0100

    style: style post preview on homepage

--- client/src/components/PostPreview/PostPreview.jsx
+++ client/src/components/PostPreview/PostPreview.jsx
@@ -3,20 +3,33 @@ import styles from './postpreview.module.css';
 import { useState, useEffect } from 'react';
 import dummyImage from '../../assets/images/hero.jpg'
 
+import Tag from '../Tag/Tag';
+
 function PostPreview() {
     const [posts, setPosts] = useState([]);
 
     useEffect(() => {
-        const fetchPosts = async () => {
-            const response = await fetch('http://localhost:3000/api/posts');
-            const result = await response.json();
-            result.forEach(post => {
-                post.content = post.content.slice('0', '300');
-            })
-            setPosts(result);
+        try {
+            const fetchPosts = async () => {
+                const response = await fetch('http://localhost:3000/api/posts');
+
+                const result = await response.json();
+
+                if (response.ok) {
+                    result.forEach(post => {
+                        post.content = post.content.slice('0', '300');
+                    })
+                    setPosts(result);
+                } else {
+                    console.log(result.message);
+                }
+            }
+
+            fetchPosts();
+        } catch (err) {
+            console.log(err);
         }
 
-        fetchPosts();
     }, [])
     return (
         <div className={styles.postsPreviewContainer}>
@@ -38,7 +51,10 @@ function Card({ post }) {
                     </div>
                     <div className={styles.content}>
                         <h3>{post.title}</h3>
-                        <span>{post.created_at}</span>
+                        <div className={styles.tags}>
+                            <Tag type="date">{post.created_at}</Tag>
+                            { post.tags.map(tag => <Tag key={tag.id} type="category">{tag.name}</Tag>)}
+                        </div>
                         <p>{post.content}</p>
                     </div>
             </article>
