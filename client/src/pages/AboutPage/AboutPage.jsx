c9acb619fd2a5b650bde6d6ff42b4fdfb50a728b
yurishe0 <yurishe00@gmail.com>
2025-01-10 00:21:03 +0100

    feat: add content to about me page

--- client/src/pages/AboutPage/AboutPage.jsx
+++ client/src/pages/AboutPage/AboutPage.jsx
@@ -1,6 +1,19 @@
 function AboutPage() {
     return (
-        <h1>about me</h1>
+        <main>
+            <p>
+                Hi, I&apos;m Dominik Augustyn, a passionate self-taught web developer from Poland. Over the past two years, I&apos;ve immersed myself in coding and exploring technologies in order to build engaging, functional, and modern web applications. My journey began with curiosity and quickly evolved into a deep passion for creation.
+            </p>
+            <p>
+                In 2023, I came across a video on social media showcasing some codebase on screen - it looked very intimidating and complex. I love learning new things, and understanding something complicated gives me satisfaction and a sense of acomplishment. That&apos;s why I decided to dive into the web development world - and it turned out to be my niche, a never ending well of knowledge. Every time I learn about something new, whether it&apos;s a function, a language or a framework - I discover more and more things that I do not know yet. And I love that!
+            </p>
+            <p>
+                I decided to turn this hobby into a passion. People online recommended me The Odin Project. It&apos;s a free curriculum that guides you in the path to become a successful web developer. It was a game-changer for me, as I was constantly stressed about whether I&apos;m going in the right direction with my studies or not.
+                </p>
+            <p>
+                Now it&apos;s 2025, and the web development community is growing larger and larger. Maybe you&apos;re a part of it! This blog is dedicated to other aspiring developers with the same goal. I want to share my journey and experiences to inspire and motivate people on the same path.
+                </p>
+        </main>
     )
 }
 
